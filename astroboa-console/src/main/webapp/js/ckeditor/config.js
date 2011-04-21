509b586dbd10178c741d6bcb7eeec2aebe5484ef
Gregory Chomatas <gchomatas@betaconcept.com>
2011-04-21 21:21:05 +0000

    Updated CKEDITOR to newer version

---
+++ astroboa-console/src/main/webapp/js/ckeditor/config.js
@@ -0,0 +1,27 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+	
+	config.toolbar = 'astroboa';
+	 
+	config.toolbar_astroboa =
+	[
+	    ['Source','-','Cut','Copy','Paste','PasteText','PasteFromWord','-','Print', 'SpellChecker', 'Scayt'],
+	    ['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
+	    ['Bold','Italic','Underline','Strike','-','Subscript','Superscript'],
+	    ['NumberedList','BulletedList','-','Outdent','Indent','Blockquote','CreateDiv'],
+	    ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
+	    ['BidiLtr', 'BidiRtl'],
+	    ['Link','Unlink','Anchor'],
+	    ['Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe'],
+	    ['Styles','Format'],
+	    ['Maximize', 'ShowBlocks','-','About']
+	];
+};
