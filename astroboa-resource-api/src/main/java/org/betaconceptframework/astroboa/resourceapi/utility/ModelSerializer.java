f76aee1a594e7782dc3a38e5345b41aa6933fa34
Savvas Triantafyllou <striantafyllou@betaconcept.com>
2011-04-21 00:51:18 +0000

    New class responsible to serialize a repository's content model

---
+++ astroboa-resource-api/src/main/java/org/betaconceptframework/astroboa/resourceapi/utility/ModelSerializer.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (C) 2005-2011 BetaCONCEPT LP.
+ *
+ * This file is part of Astroboa.
+ *
+ * Astroboa is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Astroboa is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Astroboa.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.betaconceptframework.astroboa.resourceapi.utility;
+
+import java.util.List;
+
+import org.apache.commons.lang.StringUtils;
+import org.betaconceptframework.astroboa.api.model.io.ResourceRepresentationType;
+import org.betaconceptframework.astroboa.api.service.DefinitionService;
+import org.betaconceptframework.astroboa.configuration.RepositoryRegistry;
+import org.betaconceptframework.astroboa.util.AbstractSerializer;
+
+
+/**
+ * 
+ * Class responsible to serialize the content model of a repository
+ * into one XML or JSON file
+ * 
+ * @author Gregory Chomatas (gchomatas@betaconcept.com)
+ * @author Savvas Triantafyllou (striantafyllou@betaconcept.com)
+ * 
+ */
+
+public class ModelSerializer extends AbstractSerializer{
+
+	private DefinitionService definitionService;
+	private String repository;
+	
+
+	public ModelSerializer(boolean prettyPrint, boolean jsonOutput, DefinitionService definitionService, String repository) {
+	
+		super(prettyPrint, jsonOutput);
+		
+		this.repository = repository;
+
+		this.definitionService = definitionService;
+	}
+
+	public String serialize(){
+		
+		
+		if (! outputisJSON()){
+			startElement("astroboa", true, true);
+		}
+		
+		writeAttribute("server", RepositoryRegistry.INSTANCE.getDefaultServerURL());
+		writeAttribute("repository", repository);
+		
+		if (! outputisJSON()){
+			endElement("astroboa", true, false);	
+		}
+		
+		serializeModel();
+
+		if (! outputisJSON()){
+			endElement("astroboa", false, true);
+		}
+		
+		return super.serialize();
+
+		
+	}
+
+	private void serializeModel() {
+
+		if (outputisJSON()){
+			startArray("model");
+		}
+		else{
+			startElement("model", false, true);
+		}
+		
+		List<String> contentTypes = definitionService.getContentObjectTypes();
+
+		ResourceRepresentationType<String> output = outputisJSON() ? ResourceRepresentationType.JSON : ResourceRepresentationType.XML;
+		boolean prettyPrintEnabled = prettyPrintEnabled();
+		
+		for (String contentType : contentTypes){
+			
+			String contentTypeAsXmlorJson = definitionService.getCmsDefinition(contentType, output, prettyPrintEnabled);
+		
+			if (!outputisJSON()){
+				//Remove XML headers
+				contentTypeAsXmlorJson = StringUtils.remove(contentTypeAsXmlorJson, "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>");
+			}
+			else{
+				//Remove root element
+				contentTypeAsXmlorJson = contentTypeAsXmlorJson.replaceFirst("\\{","").replaceFirst("(.*?):", "");
+				//Remove end
+				contentTypeAsXmlorJson = StringUtils.removeEnd(contentTypeAsXmlorJson, "}");
+			}
+			
+			writeContent(contentTypeAsXmlorJson, false);
+			
+		}
+		
+		if (outputisJSON()){
+			endArray();
+		}
+		else{
+			endElement("model", false, true);
+		}
+		
+	}
+}
