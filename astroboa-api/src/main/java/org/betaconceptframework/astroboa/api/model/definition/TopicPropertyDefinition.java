cba2e18de251620574ac26feac644bf3d39f4a01
Savvas Triantafyllou <striantafyllou@betaconcept.com>
2011-04-21 00:00:28 +0000

    Rename TopicPropertyDefinition to TopicReferencePropertyDefinition. Related JIRA issue ASTROBOA-8

--- astroboa-api/src/main/java/org/betaconceptframework/astroboa/api/model/definition/TopicPropertyDefinition.java
+++ astroboa-api/src/main/java/org/betaconceptframework/astroboa/api/model/definition/TopicPropertyDefinition.java
@@ -19,45 +19,13 @@
 
 package org.betaconceptframework.astroboa.api.model.definition;
 
-import java.util.List;
-import java.util.Locale;
-
 import org.betaconceptframework.astroboa.api.model.Topic;
 
 
 /**
  * Definition for a simple property whose type is {@link Topic}.
  *
-* <p>
- * Astroboa implementation uses an XML Schema <code>element</code> whose
- * <code>type</code> is Astroboa complex xml type <code>topic</code>
- * to describe simple property of type {@link Topic}.
- * 
- * The following example defines a simple property named <code>subject</code> which is
- * optional, single valued, of type {@link Topic} and whose 
- * label for {@link Locale#ENGLISH English} locale is <code>Article subject</code>.
- * 
- * <pre>
- * &lt;xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema"
- *	xmlns:bccmsmodel="http://www.betaconceptframework.org/schema/astroboa/model"&gt;
- *	
- *	&lt;xs:import
- *		namespace="http://www.betaconceptframework.org/schema/astroboa/model"
- *		schemaLocation="http://www.betaconceptframework.org/schema/astroboa/astroboa-model-{version}.xsd" /&gt;
- *  ...
- *   &lt;xs:element {@link CmsDefinition#getName() name}=&quot;subject&quot; {@link CmsPropertyDefinition#isMandatory() minOccurs}=&quot;0&quot; {@link CmsPropertyDefinition#isMultiple() maxOccurs}=&quot;1&quot;  
- *        {@link CmsDefinition#getValueType() type}=&quot;bccmsmodel:topic&quot; 
- *    &lt;{@link LocalizableCmsDefinition xs:annotation}&gt;
- *     &lt;xs:documentation xml:lang=&quot;en&quot;&gt;Article subject&lt;/xs:documentation&gt;
- *    &lt;/xs:annotation&gt;
- *   &lt;/xs:element&gt;
- *  ...
- * &lt;/xs:schema&gt;
- * </pre>
- * 
- * </p>
- * 
- * 
+ * @deprecated Use {@link TopicReferencePropertyDefinition} instead
  * @author Gregory Chomatas (gchomatas@betaconcept.com)
  * @author Savvas Triantafyllou (striantafyllou@betaconcept.com)
  * 
@@ -65,21 +33,6 @@ import org.betaconceptframework.astroboa.api.model.Topic;
  * for more on <code>topic</code> complex type. </a>
  * 
  */
-public interface TopicPropertyDefinition extends
-		SimpleCmsPropertyDefinition<Topic> {
-
-	/**
-	 * Returns a list of accepted taxonomy names which can provide property's {@link Topic} value(s).
-	 * 
-	 * <p>
-	 * Astroboa implementation uses attribute
-	 * 	<a href="http://www.betaconceptframework.org/schema/astroboa/astroboa-model-{version}.xsd">acceptedTaxonomies</a> 
-	 * to provide value for this attribute.
-	 * </p>
-
-	 * 
-	 * @return Taxonomy name restriction.
-	 */
-	List<String> getAcceptedTaxonomies();
+public interface TopicPropertyDefinition extends TopicReferencePropertyDefinition {
 
 }
