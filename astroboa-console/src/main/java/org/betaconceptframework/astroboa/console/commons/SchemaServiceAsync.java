ae78eaf140c3c9fe32be224cfd2b011c335b98f4
Gregory Chomatas <gchomatas@betaconcept.com>
2011-08-21 02:57:20 +0300

    Added async method that returns the accepted object types of an object reference property. Removed calls to deprecated Astroboa API methods

--- astroboa-console/src/main/java/org/betaconceptframework/astroboa/console/commons/SchemaServiceAsync.java
+++ astroboa-console/src/main/java/org/betaconceptframework/astroboa/console/commons/SchemaServiceAsync.java
@@ -7,7 +7,12 @@ import java.util.Map;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
 import org.betaconceptframework.astroboa.api.model.Taxonomy;
+import org.betaconceptframework.astroboa.api.model.definition.CmsPropertyDefinition;
+import org.betaconceptframework.astroboa.api.model.definition.ContentObjectTypeDefinition;
+import org.betaconceptframework.astroboa.api.model.definition.ObjectReferencePropertyDefinition;
 import org.betaconceptframework.astroboa.api.model.definition.TopicReferencePropertyDefinition;
+import org.betaconceptframework.astroboa.api.model.io.FetchLevel;
+import org.betaconceptframework.astroboa.api.model.io.ResourceRepresentationType;
 import org.betaconceptframework.astroboa.api.service.DefinitionService;
 import org.betaconceptframework.astroboa.api.service.TaxonomyService;
 import org.jboss.seam.ScopeType;
@@ -36,8 +41,10 @@ public class SchemaServiceAsync {
 		String propertyDescription[] = new String[2];
 		propertyDescription[0] = fullPropertyPath;
 		
-		propertyDescription[1] = definitionService
-			.getCmsPropertyDefinition(fullPropertyPath)
+		CmsPropertyDefinition propertyDefinition = (CmsPropertyDefinition) definitionService
+				.getCmsDefinition(fullPropertyPath, ResourceRepresentationType.DEFINITION_INSTANCE, false);
+		
+		propertyDescription[1] = propertyDefinition
 			.getDescription()
 			.getAvailableLocalizedLabel(LocaleSelector.instance()
 			.getLocaleString());
@@ -51,15 +58,16 @@ public class SchemaServiceAsync {
 		allowedTaxonomies[0] = fullPropertyPath;
 		
 		try {
-			List<String> allowedTaxonomyNames = ((TopicReferencePropertyDefinition) definitionService.getCmsPropertyDefinition(fullPropertyPath))
-			.getAcceptedTaxonomies();
+			List<String> allowedTaxonomyNames = 
+					((TopicReferencePropertyDefinition) definitionService.getCmsDefinition(fullPropertyPath, ResourceRepresentationType.DEFINITION_INSTANCE, false))
+					.getAcceptedTaxonomies();
 
 			if (CollectionUtils.isNotEmpty(allowedTaxonomyNames)) {
 
 				List<String> localizedLabels = new ArrayList<String>();
 				//Load localized Labels for all taxonomies
 				for (String allowedTaxonomyName : allowedTaxonomyNames){
-					Taxonomy allowedTaxonomy = taxonomyService.getTaxonomy(allowedTaxonomyName, null);
+					Taxonomy allowedTaxonomy = taxonomyService.getTaxonomy(allowedTaxonomyName, ResourceRepresentationType.TAXONOMY_INSTANCE, FetchLevel.ENTITY, false);
 
 					if (allowedTaxonomy == null){
 						logger.warn("Try to load accepted taxonomy {} but was not found", allowedTaxonomyName);
@@ -86,6 +94,52 @@ public class SchemaServiceAsync {
 		catch (Exception e) {
 			logger.error("An error occured while asynchronously retrieving the allowed taxonomies for a topic property", e);
 			return allowedTaxonomies;
+		}
+	}
+	
+	@WebRemote
+	public String[] getObjectRefPropertyAllowedObjectTypes(String fullPropertyPath) {
+		String allowedObjectTypes[] = new String[2];
+		allowedObjectTypes[0] = fullPropertyPath;
+		
+		try {
+			List<String> allowedObjectTypeNames = 
+					((ObjectReferencePropertyDefinition) definitionService.getCmsDefinition(fullPropertyPath, ResourceRepresentationType.DEFINITION_INSTANCE, false))
+					.getAcceptedContentTypes();
+
+			if (CollectionUtils.isNotEmpty(allowedObjectTypeNames)) {
+
+				List<String> localizedLabels = new ArrayList<String>();
+				//Load localized Labels for all taxonomies
+				for (String allowedObjectTypeName : allowedObjectTypeNames){
+					ContentObjectTypeDefinition objectTypeDefinition = 
+							(ContentObjectTypeDefinition) definitionService.getCmsDefinition(allowedObjectTypeName, ResourceRepresentationType.DEFINITION_INSTANCE, false);
+
+					if (objectTypeDefinition == null){
+						logger.warn("Try to load definition for object type {} but was not found", allowedObjectTypeName);
+						localizedLabels.add(allowedObjectTypeName);
+					}
+					else{
+						localizedLabels.add(objectTypeDefinition.getDisplayName().getAvailableLocalizedLabel(LocaleSelector.instance().getLocaleString())); 
+					}
+				}
+
+				allowedObjectTypes[1] = 
+					messages.get("content.object.edit.content.object.selection.accepted.values.specificObjectTypes") + 
+					"<br/>" +
+					"<strong>" + StringUtils.join(localizedLabels, ",") + "</strong>";
+
+			}
+			else {
+				allowedObjectTypes[1] = messages.get("content.object.edit.content.object.selection.accepted.values.allObjectTypes");
+			}
+
+
+			return allowedObjectTypes; 
+		}
+		catch (Exception e) {
+			logger.error("An error occured while asynchronously retrieving the allowed object types for an object reference property", e);
+			return allowedObjectTypes;
 		}
 	}
 	
