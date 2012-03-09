dd93b2607d9485242418049fd825c938de076fda
Savvas Triantafyllou <savvas.triantafyllou@gmail.com>
2012-03-09 17:16:14 +0200

    Ignore build_cp_ear.sh

--- astroboa-ear/build_cp_ear.sh
+++ astroboa-ear/build_cp_ear.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mvn clean package
 rm -f /opt/jboss-as-7.1.0.Final/standalone/deployments/astroboa.ear*
-cp -v target/astroboa.ear /opt/jboss-as-7.1.0.Final/standalone/deployments/
+cp -v target/astroboa.ear /Users/savvas/Projects/eclipse/workspace-3.7.x/astroboa-jboss-bundle-distribution/target/astroboa-4.0.0-SNAPSHOT/astroboa-4.0.0-SNAPSHOT/torquebox-2.0.0.cr1/jboss/standalone/deployments
