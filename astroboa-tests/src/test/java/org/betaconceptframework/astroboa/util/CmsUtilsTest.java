19ca34984c7ba5760895636a00b93526cb13fa9d
Grigorios Chomatas <gchomatas@hubspot.com>
2015-03-19 12:03:36 +0000

    Test for lowercasing / removing accents from ancient greek letters

---
+++ astroboa-tests/src/test/java/org/betaconceptframework/astroboa/util/CmsUtilsTest.java
@@ -0,0 +1,23 @@
+package org.betaconceptframework.astroboa.util;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by gchomatas on 25/01/15.
+ */
+public class CmsUtilsTest {
+
+    @Test
+    public void testToLowerCase() throws Exception {
+      Assert.assertEquals(CmsUtils.toLowerCase("ἀ".charAt(0)), "α".charAt(0));
+      Assert.assertEquals(CmsUtils.toLowerCase("Ἀ".charAt(0)), "α".charAt(0));
+      Assert.assertEquals(CmsUtils.toLowerCase("Ά".charAt(0)), "α".charAt(0));
+      Assert.assertEquals(CmsUtils.toLowerCase("ῖ".charAt(0)), "ι".charAt(0));
+      Assert.assertEquals(CmsUtils.toLowerCase("ὕ".charAt(0)), "υ".charAt(0));
+      Assert.assertEquals(CmsUtils.toLowerCase("ὁ".charAt(0)), "ο".charAt(0));
+      Assert.assertEquals(CmsUtils.toLowerCase("ἡ".charAt(0)), "η".charAt(0));
+
+    }
+
+}
